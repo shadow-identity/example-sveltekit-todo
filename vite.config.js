d09db3764e89913f55524e0392b2806ea1cfa351
Pavel Nedrigailov <pavel.nedrigailov@invenium.io>
2024-06-07 12:42:56 +0200

    Update houdini, add vite plugin

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { sveltekit } from '@sveltejs/kit/vite'
 import houdini from 'houdini/vite'
 import path from 'path'
+import circleDependency from 'vite-plugin-circular-dependency'
 
 /** @type {import('vite').UserConfig} */
 const config = {
-	plugins: [houdini(), sveltekit()],
+	plugins: [houdini(), sveltekit(), circleDependency()],
 }
 
 export default config
